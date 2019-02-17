0a472bd31b0f1e885b63ccc42e37ce2cdd435be9
Zach Saffrin <zach@zachsaffrin.com>
2019-02-17 14:54:26 -0500

    Define colors discreetly

--- src/themes/spiff.js
+++ src/themes/spiff.js
@@ -1,4 +1,20 @@
 const renderTheme = (baseFontSize = 16) => {
+  const colors = {
+    black: '#111',
+    grays: ['#ebedee', '#dee1e3', '#bec4c8', '#97a1a7', '#5f6e78'],
+    white: '#f8f9f9',
+    primary: '#00365d',
+    secondary: '#610008',
+    danger: '#b8000f',
+    success: '#00a243',
+  };
+
+  const inputs = {
+    background: colors.white,
+    border: `1px solid ${colors.grays[2]}`,
+    borderRadius: '3px',
+  };
+
   const spacing = {
     xs: baseFontSize * 0.125, // 2px
     sm: baseFontSize * 0.25, //  4px
@@ -13,20 +29,13 @@ const renderTheme = (baseFontSize = 16) => {
     bodyFontFamily:
       "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
     baseFontSize,
-    colors: {
-      black: '#111',
-      grays: ['#ebedee', '#dee1e3', '#bec4c8', '#97a1a7', '#5f6e78'],
-      white: '#f8f9f9',
-      primary: '#00365d',
-      secondary: '#610008',
-      danger: '#b8000f',
-      success: '#00a243',
-    },
+    colors,
     spacing,
     buttons: {
       corners: '3px',
       fontWeight: 'bold',
     },
+    inputs,
   };
 };
 
