2d87ca2eea92d027a479127ec21100e6e91c0707
Zach Saffrin <zach@zachsaffrin.com>
2019-02-23 15:23:50 -0500

    Accept buttons, force border radius style

--- src/components/ButtonGroup/ButtonGroup.js
+++ src/components/ButtonGroup/ButtonGroup.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { arrayOf } from 'prop-types';
+import { arrayOf, node } from 'prop-types';
+import styled from 'styled-components';
 
 const ButtonGroup = ({ items }) => {
-  const listItems = items.map(i => <li>{i}</li>);
+  const StyledGroup = styled.ul`
+    list-style: none;
+  `;
 
-  return <ul>{listItems}</ul>;
+  const StyledItem = styled.li`
+    display: inline-block;
+
+    & > button {
+      border-radius: 0 !important;
+    }
+
+    &:first-child > button {
+      border-radius: 8px 0 0 8px !important;
+    }
+    &:last-child > button {
+      border-radius: 0 8px 8px 0 !important;
+    }
+  `;
+
+  const listItems = items.map(i => <StyledItem>{i}</StyledItem>);
+
+  return <StyledGroup>{listItems}</StyledGroup>;
 };
 ButtonGroup.propTypes = {
-  items: arrayOf(),
+  items: arrayOf(node),
 };
 ButtonGroup.defaultProps = {
   items: [],
