45b571cf6547830254abdc7b652f782cc2e51744
Zach Saffrin <6954752+zsaffrin@users.noreply.github.com>
2024-01-17 21:47:20 -0500

    Establish App component tree

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>🚀 Spiff UI</h1>
+      <p>A handy little React UI component kit and design system</p>
+    </div>
+  );
+};
+
+export default App;
