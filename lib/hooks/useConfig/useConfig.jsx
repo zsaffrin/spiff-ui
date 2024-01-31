4dad9dcb4e13f27ed18ef14cbfe1813b0f28aa93
Zach Saffrin <6954752+zsaffrin@users.noreply.github.com>
2024-01-30 21:20:42 -0500

    Use merge util to compose config

--- lib/hooks/useConfig/useConfig.jsx
+++ lib/hooks/useConfig/useConfig.jsx
@@ -1,14 +1,12 @@
 import { useContext } from 'react';
 import { ConfigContext } from '../../contexts';
+import { merge } from '../../utils';
 import defaultConfig from './defaultConfig';
 
 const useConfig = () => {
   const configArgs = useContext(ConfigContext);
 
-  const composedConfig = {
-    ...defaultConfig,
-    ...configArgs,
-  };
+  const composedConfig = merge(defaultConfig, configArgs);
 
   return composedConfig;
 };
