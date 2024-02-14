033f3d428d8e827603910776f34de6d76e59f418
Zach Saffrin <6954752+zsaffrin@users.noreply.github.com>
2024-02-14 17:34:00 -0500

    Show code for examples

--- src/components/App/Content/ComponentDemos/ButtonDemo/ButtonDemo.jsx
+++ src/components/App/Content/ComponentDemos/ButtonDemo/ButtonDemo.jsx
@@ -1,5 +1,15 @@
+import styled from 'styled-components';
+
 import { Button } from '../../../../../../';
-import { Section } from '../../../../ui';
+import { CodeBlock, Section } from '../../../../ui';
+
+const Row = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: min-content;
+  align-items: center;
+  grid-gap: 0.5rem;
+`;
 
 const ButtonDemo = () => {
   return (
@@ -7,14 +17,24 @@ const ButtonDemo = () => {
       <Section>
         <p>Default Button</p>
         <Button />
+        <CodeBlock content={`<Button />`} />
       </Section>
       <Section>
         <p>Button sizes</p>
-        <Button size="xs" />
-        <Button size="sm" />
-        <Button />
-        <Button size="lg" />
-        <Button size="xl" />
+        <Row>
+          <Button size="xs" />
+          <Button size="sm" />
+          <Button />
+          <Button size="lg" />
+          <Button size="xl" />
+        </Row>
+        <CodeBlock content={`<>
+  <Button size="xs" />
+  <Button size="sm" />
+  <Button />
+  <Button size="lg" />
+  <Button size="xl" />
+</>`} />
       </Section>
     </>
   );
