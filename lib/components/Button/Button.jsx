2a364c555a9c9b48f8e234f5c8e2bba8a67d2b5e
Zach Saffrin <6954752+zsaffrin@users.noreply.github.com>
2024-02-13 22:31:16 -0500

    Setup button sizes

--- lib/components/Button/Button.jsx
+++ lib/components/Button/Button.jsx
@@ -1,26 +1,57 @@
+import { string } from 'prop-types';
 import styled from 'styled-components';
 import { useConfig } from '../../hooks';
 
-const StyledButton = styled.button(({ $config }) => {
+const StyledButton = styled.button(({
+  $config,
+  size,
+}) => {
+  let fontSize = '0.79rem';
+  let padding = '0.5em 1em';
+  if (size === 'xs') {
+    fontSize = '0.68rem';
+    padding = '0 0.25em';
+  }
+  if (size === 'sm') {
+    fontSize = '0.71rem';
+    padding = '0.25em 0.5em';
+  }
+  if (size === 'lg') {
+    fontSize = '0.86rem';
+    padding = '0.75em 1.25em';
+  }
+  if (size === 'xl') {
+    fontSize = '0.9rem';
+    padding = '1em 1.5em';
+  }
+
   return `
     appearance: none;
     border-radius: ${$config.buttonBorderRadius};
-    border-style: ${$config.buttonBorderStyle};
-    border-width: ${$config.buttonBorderWidth};
+    border-style: solid;
+    border-width: 1px;
     cursor: pointer;
+    font-size: ${fontSize};
     font-weight: bold;
-    padding: ${$config.buttonPadding};
+    padding: ${padding};
   `;
 });
 
-const Button = () => {
+const Button = (props) => {
   const config = useConfig();
 
   return (
-    <StyledButton $config={config}>
+    <StyledButton
+      $config={config}
+      {...props}
+    >
       Button
     </StyledButton>
   );
 };
+Button.propTypes = {
+  size: string,
+};
+Button.defaultProps = {};
 
 export default Button;
